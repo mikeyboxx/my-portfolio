7593c2bfdf0cb6dcbd76ebcab1054ac11fea4d08
Michael Nabatov <happyanddebtfree@gmail.com>
2022-12-03 17:09:07 -0500

    Added Navigation component to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Navigation from './Navigation'
 
+const style = {
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  }
+}
 const Header = () => {
   return (
-    <div>Header</div>
+    <div style={style.header}>
+      <div>
+        Michael Nabatov
+      </div>
+      <Navigation />
+    </div>
   )
 }
 
