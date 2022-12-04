74ce1608007657866d0a63ecf71aab20bd7c3317
Michael Nabatov <happyanddebtfree@gmail.com>
2022-12-04 13:24:32 -0500

    Created projects.js containing an array of objects containing project data

---
+++ src/utils/projects.js
@@ -0,0 +1,47 @@
+const projects = [
+  {
+    title: 'Socialize',
+    techUsed: ['HTML', 'CSS' ,'Javascript', 'Node', 'Express', 'MySql'],
+    imageSrc: '../images/screenshot-of-socialize-dashboard.png',
+    deployedUrl: 'https://pure-depths-45442.herokuapp.com/',
+    githubRepoUrl: 'https://github.com/mikeyboxx/socialize',
+  },
+  {
+    title: 'My Daily Almanac',
+    techUsed: ['HTML', 'CSS' ,'Javascript'],
+    imageSrc: '../images/screenshot-of-my-daily-almanac.png',
+    deployedUrl: 'https://mikeyboxx.github.io/my-daily-almanac/',
+    githubRepoUrl: 'https://github.com/mikeyboxx/my-daily-almanac/'
+  },
+  {
+    title: 'CSS Snippets',
+    techUsed: ['HTML', 'CSS'],
+    imageSrc: 'https://picsum.photos/200/200',
+    deployedUrl: 'https://mikeyboxx.github.io/css-snippets/',
+    githubRepoUrl: 'https://github.com/mikeyboxx/css-snippets/'
+  },
+  {
+    title: 'Study Guide',
+    techUsed: ['HTML', 'CSS', 'Javascript'],
+    imageSrc: 'https://picsum.photos/200/200',
+    deployedUrl: 'https://mikeyboxx.github.io/prework-study-guide/',
+    githubRepoUrl: 'https://github.com/mikeyboxx/prework-study-guide/'
+  },
+  {
+    title: 'Crypto Watch',
+    techUsed: ['HTML', 'CSS', 'Javascript'],
+    imageSrc: 'https://picsum.photos/200/200',
+    deployedUrl: 'https://mikeyboxx.github.io/prework-study-guide/',
+    githubRepoUrl: 'https://github.com/mikeyboxx/prework-study-guide/'
+  },
+  {
+    title: 'Future Project',
+    techUsed: ['HTML', 'CSS'],
+    imageSrc: 'https://picsum.photos/200/200',
+    deployedUrl: 'https://mikeyboxx.github.io/prework-study-guide/',
+    githubRepoUrl: 'https://github.com/mikeyboxx/prework-study-guide/'
+  },
+]
+
+
+export default projects;
