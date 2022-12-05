8c0c51a30ab75c6f5dadf1c9d7de0129ab9b13e4
Michael Nabatov <happyanddebtfree@gmail.com>
2022-12-05 17:57:58 -0500

    Added code to Portfolio.js and Project.js to render a list of projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,12 +1,29 @@
 import projects from '../utils/projects';
 import Project from './Project';
 
+const style={
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    // padding: '2%',
+  }
+}
+
 const Portfolio = () => {
   return (
-    <div className='d-flex ml-3'>
-      {projects.map((project)=>(
-        <Project project={project} />
-      ))}
+    // <div className='d-flex ml-3'>
+    <div className="card w-100 p-2" >
+      <div className="card-header text-start">
+          <h4>Portfolio</h4>
+      </div>
+      <div className="card flex-row flex-wrap">
+
+        {projects.map((project, idx)=>(
+          <Project key={idx} project={project} />
+        ))}
+
+      </div>
     </div>
   )
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,83 @@
-import React from 'react'
+import {FaGithub}  from "react-icons/fa";
+import { useState } from "react";
 
-const Project = () => {
+const Project = ({ project: {title, techUsed, imageSrc, deployedUrl, githubRepoUrl}}) => {
+  const [isHoveredImage, setIsHoveredImage] = useState(false);
+  const [isHoveredTitle, setIsHoveredTitle] = useState(false);
+  const [isHoveredIcon, setIsHoveredIcon] = useState(false);
+
+  const style = {
+    container: {
+      flex: '1 0 45%',
+      position: 'relative',
+      boxShadow: '12px 15px 4px rgba(0, 0, 0, 0.25)',
+      borderRadius: '10px',
+      margin: '20px',
+    },
+    image: {
+      borderRadius: '15px',
+      height: '400px',
+      width: '100%',
+      opacity: isHoveredImage ? '.4' : '1',
+      objectFit: 'cover',
+      objectPosition: 'left top'
+    },
+    icon: {
+      color: isHoveredIcon ? 'grey' : 'black',
+      cursor: isHoveredIcon ? 'hand' : 'pointer',
+      fontSize: '1.5rem',
+      marginLeft: 15,
+      marginTop: 10,
+    },
+    title: {
+      color: isHoveredTitle ? 'grey' : 'black',
+      cursor: isHoveredTitle ? 'hand' : 'pointer',
+    },
+    techUsed: {
+      fontSize: '.9em',
+      fontWeight: 500
+    },
+    caption: {
+      display: 'flex',
+      alignItems: 'start',
+      flexFlow: 'column',
+      position: 'absolute',
+      top: 50,
+      left: 30,
+      color: 'black',
+      fontStyle: 'italic',
+      visibility: !isHoveredImage ? 'hidden' : 'visible',
+    }
+  }
+  
   return (
-    <div>Project</div>
+    <section style={style.container} 
+      onMouseOver={() => setIsHoveredImage(true)} 
+      onMouseOut={() => setIsHoveredImage(false)} 
+    >
+      <img 
+        style={style.image} 
+        src={imageSrc} 
+        alt={`${title} website`} 
+      />
+      <div style={style.caption}>
+        <div className='d-flex'>
+          <h2 style={style.title} 
+            onMouseOver={() => setIsHoveredTitle(true)} 
+            onMouseOut={() => setIsHoveredTitle(false)}
+            onClick={()=>window.open(deployedUrl, '_blank')}>
+              {title}
+          </h2>
+          <FaGithub style={style.icon} 
+            onMouseOver={() => setIsHoveredIcon(true)} 
+            onMouseOut={() => setIsHoveredIcon(false)}
+            onClick={()=>window.open(githubRepoUrl, '_blank')}
+          />
+        </div>
+        <p style={style.techUsed}>{techUsed.join('/')}</p>
+      </div>
+    </section>
   )
 }
 
-export default Project
+export default Project;
