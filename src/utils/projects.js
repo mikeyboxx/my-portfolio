08e2ee7ceb4644b9eddcbb1d182d604167a81fe7
Michael Nabatov <happyanddebtfree@gmail.com>
2022-12-05 22:49:29 -0500

    Added README file

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -8,61 +8,40 @@ const projects = [
     githubRepoUrl: 'https://github.com/mikeyboxx/socialize',
   },
   {
-    title: 'Socialize',
-    techUsed: ['HTML', 'CSS' ,'Javascript', 'Node', 'Express', 'MySql'],
-    imageSrc: require('../images/screenshot-of-socialize-dashboard.png'),
-    deployedUrl: 'https://pure-depths-45442.herokuapp.com/',
-    githubRepoUrl: 'https://github.com/mikeyboxx/socialize',
-  },
-  {
-    title: 'Socialize',
-    techUsed: ['HTML', 'CSS' ,'Javascript', 'Node', 'Express', 'MySql'],
-    imageSrc: require('../images/screenshot-of-socialize-dashboard.png'),
-    deployedUrl: 'https://pure-depths-45442.herokuapp.com/',
-    githubRepoUrl: 'https://github.com/mikeyboxx/socialize',
-  },
-  {
-    title: 'Socialize',
-    techUsed: ['HTML', 'CSS' ,'Javascript', 'Node', 'Express', 'MySql'],
-    imageSrc: require('../images/screenshot-of-socialize-dashboard.png'),
-    deployedUrl: 'https://pure-depths-45442.herokuapp.com/',
-    githubRepoUrl: 'https://github.com/mikeyboxx/socialize',
-  },
-  {
     title: 'My Daily Almanac',
     techUsed: ['HTML', 'CSS' ,'Javascript'],
     imageSrc: require('../images/screenshot-of-my-daily-almanac.png'),
     deployedUrl: 'https://mikeyboxx.github.io/my-daily-almanac/',
     githubRepoUrl: 'https://github.com/mikeyboxx/my-daily-almanac/'
   },
-  // {
-  //   title: 'CSS Snippets',
-  //   techUsed: ['HTML', 'CSS'],
-  //   imageSrc: 'https://picsum.photos/200/200',
-  //   deployedUrl: 'https://mikeyboxx.github.io/css-snippets/',
-  //   githubRepoUrl: 'https://github.com/mikeyboxx/css-snippets/'
-  // },
-  // {
-  //   title: 'Study Guide',
-  //   techUsed: ['HTML', 'CSS', 'Javascript'],
-  //   imageSrc: 'https://picsum.photos/200/200',
-  //   deployedUrl: 'https://mikeyboxx.github.io/prework-study-guide/',
-  //   githubRepoUrl: 'https://github.com/mikeyboxx/prework-study-guide/'
-  // },
-  // {
-  //   title: 'Crypto Watch',
-  //   techUsed: ['HTML', 'CSS', 'Javascript'],
-  //   imageSrc: 'https://picsum.photos/200/200',
-  //   deployedUrl: 'https://mikeyboxx.github.io/prework-study-guide/',
-  //   githubRepoUrl: 'https://github.com/mikeyboxx/prework-study-guide/'
-  // },
-  // {
-  //   title: 'Future Project',
-  //   techUsed: ['HTML', 'CSS'],
-  //   imageSrc: 'https://picsum.photos/200/200',
-  //   deployedUrl: 'https://mikeyboxx.github.io/prework-study-guide/',
-  //   githubRepoUrl: 'https://github.com/mikeyboxx/prework-study-guide/'
-  // },
+  {
+    title: 'CSS Snippets',
+    techUsed: ['HTML', 'CSS'],
+    imageSrc: 'https://picsum.photos/200/200',
+    deployedUrl: 'https://mikeyboxx.github.io/css-snippets/',
+    githubRepoUrl: 'https://github.com/mikeyboxx/css-snippets/'
+  },
+  {
+    title: 'Study Guide',
+    techUsed: ['HTML', 'CSS', 'Javascript'],
+    imageSrc: 'https://picsum.photos/200/200',
+    deployedUrl: 'https://mikeyboxx.github.io/prework-study-guide/',
+    githubRepoUrl: 'https://github.com/mikeyboxx/prework-study-guide/'
+  },
+  {
+    title: 'Crypto Watch',
+    techUsed: ['HTML', 'CSS', 'Javascript'],
+    imageSrc: 'https://picsum.photos/200/200',
+    deployedUrl: 'https://mikeyboxx.github.io/prework-study-guide/',
+    githubRepoUrl: 'https://github.com/mikeyboxx/prework-study-guide/'
+  },
+  {
+    title: 'Future Project',
+    techUsed: ['HTML', 'CSS'],
+    imageSrc: 'https://picsum.photos/200/200',
+    deployedUrl: 'https://mikeyboxx.github.io/prework-study-guide/',
+    githubRepoUrl: 'https://github.com/mikeyboxx/prework-study-guide/'
+  },
 ]
 
 
