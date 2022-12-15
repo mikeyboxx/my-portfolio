975e0e4b1b9f00a5e32d573a49183e045b16a2b5
Michael Nabatov <happyanddebtfree@gmail.com>
2022-12-15 15:34:42 -0500

    Added virtual escape room project

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
 
+
+
 function App() {
   const [currentPage, setCurrentPage] = useState('aboutme');
   
--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,11 +1,12 @@
 
 const projects = [
+  
   {
-    title: 'Socialize',
-    techUsed: ['HTML', 'CSS' ,'Javascript', 'Node', 'Express', 'MySql'],
-    imageSrc: require('../images/screenshot-of-socialize-dashboard.png'),
-    deployedUrl: 'https://pure-depths-45442.herokuapp.com/',
-    githubRepoUrl: 'https://github.com/mikeyboxx/socialize',
+    title: 'Virtual Escape Room',
+    techUsed: ['HTML', 'React', 'CSS' ,'Javascript'],
+    imageSrc: require('../images/screenshot-of-virtual-escape-room.png'),
+    deployedUrl: 'https://virtual-escape-room-mikeyboxx.herokuapp.com/',
+    githubRepoUrl: 'https://github.com/mikeyboxx/virtual-escape-room'
   },
   {
     title: 'My Daily Almanac',
@@ -13,6 +14,13 @@ const projects = [
     imageSrc: require('../images/screenshot-of-my-daily-almanac.png'),
     deployedUrl: 'https://mikeyboxx.github.io/my-daily-almanac/',
     githubRepoUrl: 'https://github.com/mikeyboxx/my-daily-almanac/'
+  },
+  {
+    title: 'Socialize',
+    techUsed: ['HTML', 'CSS' ,'Javascript', 'Node', 'Express', 'MySql'],
+    imageSrc: require('../images/screenshot-of-socialize-dashboard.png'),
+    deployedUrl: 'https://pure-depths-45442.herokuapp.com/',
+    githubRepoUrl: 'https://github.com/mikeyboxx/socialize',
   },
   {
     title: 'CSS Snippets',
